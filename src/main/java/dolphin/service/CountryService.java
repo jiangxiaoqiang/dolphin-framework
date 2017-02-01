f38c2b0550de25d803028eff5326629c22720fbf
jiangxiaoqiang <jiangtingqiang@gmail.com>
2017-02-01 22:13:43 +0800

    daily

--- src/main/java/dolphin/service/CountryService.java
+++
@@ -1,7 +0,0 @@
-package dolphin.service;
-
-import dolphin.mode.Country;
-
-public interface CountryService {
-	Country getCountyById(int id);
-}
--- src/main/java/dolphin/test/Book.java
+++
@@ -1,9 +0,0 @@
-package dolphin.test;
-
-/**
- * @Author: jiangxiaoqiang
- * Created by 12/13/16 on 5:08 PM.
- */
-public interface Book {
-    public void addBook();
-}
